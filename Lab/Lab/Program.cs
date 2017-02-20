426b5644860d6f65bf253ae73558ff6871c00ae9
timursaet <s.timursaet@yandex.ru>
2017-02-20 15:29:56 +0500

    Лабораторная работа 5 на C#

--- Lab/Lab/Program.cs
+++ Lab/Lab/Program.cs
@@ -10,34 +10,25 @@ namespace Lab
     {
         static void Main(string[] args)
         {
-            int n, f, i;
-            double sum;
-            Console.WriteLine("Введите N");
+            int n;
+            int x, max, min;
+
+            Console.WriteLine("Введите число");
             n = (int) Console.Read();
-            sum = 0; f = 1;
-            for (i = 1; i < n; i++) {
-                sum = sum + ((Math.Pow(f, i + 1)) / (i * (i + 1)));
-                f = -f;
-            }
-            Console.WriteLine("Сумма с циклом for = "+sum);
-            i = 1; sum = 0; f = 1;
-            while (i <= n) {
-                sum = sum + ((Math.Pow(f, i + 1)) / (i * (i + 1)));
-                f = -f;
-                i++;
-            }
-            Console.WriteLine("Сумма с циклом while = " + sum);
-            i = 1; sum = 0; f = 1;
-            do {
-                sum = sum + ((Math.Pow(f, i + 1)) / (i * (i + 1)));
-                f = -f;
-                i++;
-            }
-            while (i<=n);
-            Console.WriteLine("Сумма с циклом do while = " + sum);
 
+            min = 9; max = 0;
 
-        Console.ReadKey();
+            while (n>0) {
+                x =n % 10;
+                n = n / 10;
+                if (x < min) min = x;
+                if (x > max) max = x;
+            }
+
+            Console.WriteLine("Наибольшая цифра"+max);
+            Console.WriteLine("Наименьшая цифра" + min);
+
+            Console.ReadKey();
         }  
     }
 }
