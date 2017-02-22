ff27b53630775cfe106c8fec3425a0d30e8a36eb
timursaet <s.timursaet@yandex.ru>
2017-02-22 13:20:09 +0600

    Update Program.cs

--- Lab/Lab/Program.cs
+++ Lab/Lab/Program.cs
@@ -10,21 +10,22 @@ namespace Lab
     {
         static void Main(string[] args)
         {
-            const int n = 10;
-            const int m = 10;
-            int[][] a = new int [n][m];
+            const int n = 5;
+            int[,] a = new int [n+1,n+1];
             int i, j;
-            for (i = 1; i < n; i++)
-                for (j = 1; j < n; j++) {
-                    a[i][j] = 1;
+            for (i = 1; i <= n; i++)
+                for (j = 1; j <= n; j++) {
+                    a[i,j] = 1;
                 }
-            for (i = 2; i < n - 1; i++) {
-                a[i][1] = 0; a[i][n] = 0;
+            for (i = 2; i <= n - 1; i++) {
+                a[i,1] = 0; a[i,n] = 0;
             }
-            for (i = 1; i < n; i++) {
-                for (j = 1; j < m; j++)
-                    Console.WriteLine(a[i][j]);
-                Console.ReadLine();
+            for (i = 1; i <= n; i++) {
+                for (j = 1; j <= n; j++)
+                {
+                    Console.Write(a[i, j]);
+                } 
+                Console.WriteLine();
             }
             Console.ReadKey();
         }  
