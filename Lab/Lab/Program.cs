b1bc7c7aff1cc35cd11583bff0dafff8127155b0
timursaet <s.timursaet@yandex.ru>
2017-03-01 00:31:38 +0500

    Лабораторная работа №8 на C#

--- Lab/Lab/Program.cs
+++ Lab/Lab/Program.cs
@@ -10,19 +10,45 @@ namespace Lab
     {
         static void Main(string[] args)
         {
-            int x, y;
-            Console.WriteLine("Введите кординаты точек x и y");
-            x = (int) Console.Read();
-            y = (int)Console.Read();
-            if (((x >= -2) & (x <= -1) & (y >= 0) & (y <= 1)) |
-              ((x >= 1) & (x <= 2) & (y >= -2) & (y <= 2)) | ((x >= 1) & (x <= 2) & (y >= 0) & (y <= 1)))
-            {
-                Console.WriteLine("Точка пренадлежит данной области");
+            int i, j, n, m, tmp;
+            int max, iPos, jPos;
+            int[,] a = new int[100,100];
+
+            Console.WriteLine("Введите размерность массива");
+            n = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+            Random b = new Random();
+
+            max = 0;
+            Console.WriteLine("Исходная матрица");
+            for (i = 1; i < n; i++) {
+                for (j = 1; j < m; j++) {
+                    a[i, j] = b.Next(30);
+                    if (a[i, j] > max) {
+                        max = a[i,j]; iPos = i; jPos = j;
+                    }
+                    Console.Write(a[i,j]);
+                }
+                Console.WriteLine();
             }
-            else Console.WriteLine("Точка не пренадлежит данной области");
+            Console.WriteLine("Максимальный элемент находится на a["+iPos+","+jPos+"]="+max);
+            for (i = 1; i < n; i++) {
+                tmp = a[i, 1]; a[i, 1] = a[i, jPos]; a[i, jPos] = tmp;
+            }
+            for (j = 1; i < n; j++)
+            {
+                tmp = a[1, j]; a[1, j] = a[iPos, j]; a[iPos,j] = tmp;
+            }
+            Console.WriteLine("Результат матрицы");
+            for (i = 1; i < n; i++) {
+                for (j = 1; j < m; j++) {
+                    Console.Write(a[i,j]);
+                }
+                Console.WriteLine();
+            }
 
 
-         Console.ReadKey();
+            Console.ReadKey();
         }  
     }
 }
